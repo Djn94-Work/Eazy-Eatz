7dd715080f9f1f3542bb6c8874fbea7b6bd3c051
Don Newman <dnewman@talentpath.com>
2019-12-02 16:15:11 -0600

    food species

--- ezeatz/src/header/Header.js
+++ ezeatz/src/header/Header.js
@@ -3,20 +3,23 @@ import AddressForm from "./AddressForm";
 import FilterPannel from "../mainpage/FilterPannel/FilterPannel";
 import Select from "@material-ui/core/Select";
 import "./Header.css";
+import { InputLabel, TextField } from "@material-ui/core";
 
 function Header(props) {
   return (
     <div className="Header">
       <AddressForm
+        className="searchBar"
         handleSubmit={props.handleSubmit}
         handleOnChange={props.handleOnChange}
       ></AddressForm>
+      <InputLabel id="filter-label">Food Species</InputLabel>
       <Select labelId="filter-label" id="filter">
         <FilterPannel
           handleSlide={props.handleSlide}
           handleFilter={props.handleFilter}
           radius={props.radius}
-        />
+        ></FilterPannel>
       </Select>
     </div>
   );
