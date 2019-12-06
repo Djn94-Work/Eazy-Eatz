f02ade58a349ab1e7dfa9c48acb4c60d0780d8d0
Don Newman <dnewman@talentpath.com>
2019-12-06 10:37:55 -0600

    buttonBuilder

--- ezeatz/src/menupage/menuSection/subCat/subCat.js
+++ ezeatz/src/menupage/menuSection/subCat/subCat.js
@@ -1,42 +1,24 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 function Subcat(props) {
-  return (
-    <div>
-      <Button
-        variant="contained"
-        onClick={() => {
-          props.handleButton("Appetizers");
-        }}
-      >
-        {props.subCatArray[0]}
-      </Button>
-      <Button
-        variant="contained"
-        onClick={() => {
-          props.handleButton("Enchiladas");
-        }}
-      >
-        {props.subCatArray[1]}
-      </Button>
-      <Button
-        variant="contained"
-        onClick={() => {
-          props.handleButton("Fajitas");
-        }}
-      >
-        {props.subCatArray[2]}
-      </Button>
-      <Button
-        variant="contained"
-        onClick={() => {
-          props.handleButton("Desert");
-        }}
-      >
-        {props.subCatArray[3]}
-      </Button>
-    </div>
-  );
+  const buttonBuilder = () => {
+    const buttonArray = [];
+    for (const subCat of props.subCatArray) {
+      buttonArray.push(
+        <Button
+          variant="contained"
+          onClick={() => {
+            props.handleButton(subCat);
+          }}
+        >
+          {subCat}
+        </Button>
+      );
+    }
+    return buttonArray;
+  };
+
+  return <div>{buttonBuilder()}</div>;
 }
 
 export default Subcat;
