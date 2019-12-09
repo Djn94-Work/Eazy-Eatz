58bf8df1159a826cf552d0620163a66d6c46cf23
Don Newman <dnewman@talentpath.com>
2019-12-09 08:37:01 -0600

    NameTag Component

--- ezeatz/src/header/Header.js
+++ ezeatz/src/header/Header.js
@@ -2,23 +2,27 @@ import React from "react";
 import AddressForm from "./AddressForm";
 import FilterPannel from "./FilterPannel/FilterPannel";
 import Select from "@material-ui/core/Select";
+import NameTag from "./NameTag";
 import "./Header.css";
 //import { InputLabel, TextField } from "@material-ui/core";
 
 function Header(props) {
   return (
     <div className="Header">
-      <AddressForm
-        className="searchBar"
-        handleSubmit={props.handleSubmit}
-      ></AddressForm>
-      <Select labelId="filter-label" id="filter" autoWidth={true}>
-        <FilterPannel
-          handleSlide={props.handleSlide}
-          handleFilter={props.handleFilter}
-          radius={props.radius}
-        ></FilterPannel>
-      </Select>
+      <div className="addressAndFilter">
+        <AddressForm
+          className="searchBar"
+          handleSubmit={props.handleSubmit}
+        ></AddressForm>
+        <Select labelId="filter-label" id="filter" autoWidth={true}>
+          <FilterPannel
+            handleSlide={props.handleSlide}
+            handleFilter={props.handleFilter}
+            radius={props.radius}
+          ></FilterPannel>
+        </Select>
+      </div>
+      <NameTag />
     </div>
   );
 }
---
+++ ezeatz/src/header/NameTag.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./NameTag.css";
+function NameTag(props) {
+  return <h1 className="nameTag">EaZy-EAtZ</h1>;
+}
+
+export default NameTag;
